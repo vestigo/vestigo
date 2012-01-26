3dc9121b92cd2adcf8233f27074dcd14e5d6ce52
fleque <fleque@users.sourceforge.net>
2012-01-26 11:00:17 +0000

    Intial commit of cumulus4j webapp shared project

---
+++ org.nightlabs.jjqb.cumulus4j.childvm.shared/src/main/java/org/nightlabs/jjqb/cumulus4j/childvm/shared/JDOCumulus4JConnectionDTO.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.nightlabs.jjqb.cumulus4j.childvm.shared;
+
+import org.nightlabs.jjqb.childvm.shared.JDOConnectionDTO;
+
+/**
+ * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
+ *
+ */
+public class JDOCumulus4JConnectionDTO extends JDOConnectionDTO {
+
+	private static final long serialVersionUID = 20111229L;
+
+}
