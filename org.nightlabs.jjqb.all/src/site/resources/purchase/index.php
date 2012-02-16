bd0674e421dc2cdf90f50b0fcd28e7dad9ee571a
mschulze <marco@nightlabs.de>
2012-02-16 13:49:05 +0000

    Added site.

---
+++ org.nightlabs.jjqb.all/src/site/resources/purchase/index.php
@@ -0,0 +1,23 @@
+<?php
+header("Location: https://secure.payproglobal.com/orderpage.aspx?products=87920");
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <style type="text/css" media="all">
+      @import url("../css/maven-base.css");
+      @import url("../css/maven-theme.css");
+      @import url("../css/site.css");
+    </style>
+    <link rel="stylesheet" href="../css/print.css" type="text/css" media="print" />
+    <meta name="author" content="Marco หงุ่ยตระกูล-Schulze" />
+    <meta name="Date-Revision-yyyymmdd" content="20120216" />
+    <meta http-equiv="Content-Language" content="en" />
+    <title>JDO/JPA Query Browser (JJQB) - Purchase JJQB</title>
+  </head>
+  <body>
+    You should be forwarded to the shop automatically. If you are not, please
+    <a href="https://secure.payproglobal.com/orderpage.aspx?products=87920">click here</a>. 
+  </body>
+</html>
