94d2c35b124be39c4dfc2a0b0cb126bd2a06d877
fleque <fleque@users.sourceforge.net>
2012-04-12 14:06:14 +0000

    Changed homepage to new design (first version)

---
+++ org.nightlabs.vestigo.all/src/site/resources/vestigo-slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){	
+	  $("#slider").easySlider({
+		  auto: true, 
+		  pause: 8000,
+		  continuous: true,
+		  prevText: '',
+		  nextText: '',
+		  controlsFade: false
+	  });
+});	
