dd6a777afe1c1a83f26fd229d7479018ded3c00c
mschulze <marco@nightlabs.de>
2012-04-24 21:33:54 +0000

    Updated howto and extended VersionUpdater to process the MANIFEST.MF files, too.

--- org.nightlabs.vestigo.release/src/main/java/org/nightlabs/vestigo/release/VersionUpdater.java
+++ org.nightlabs.vestigo.release/src/main/java/org/nightlabs/vestigo/release/VersionUpdater.java
@@ -1,6 +1,15 @@
 package org.nightlabs.vestigo.release;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,39 +28,91 @@ import org.w3c.dom.NodeList;
 
 public class VersionUpdater {
 
-	private String newMavenVersion = "0.7.0-SNAPSHOT";
-	private String artifactIdPrefix = "org.nightlabs.vestigo.";
+	protected String newMavenVersion = "0.7.0-SNAPSHOT";
 
-	private String newOsgiVersionWithoutSuffix;
-	private String newOsgiVersion;
+	protected String artifactIdPrefix = "org.nightlabs.vestigo.";
 
-	private String rootDir = "..";
+	protected String newOsgiVersionWithoutSuffix;
+	protected String newOsgiVersionWithQualifier;
+	protected String newOsgiVersionWithSnapshot;
 
-	private List<File> pomFiles;
-	private List<File> featureFiles;
-	private List<File> categoryFiles;
+	protected String rootDir = "..";
 
-	private VersionUpdater() { }
+	protected List<File> pomFiles;
+	protected List<File> featureFiles;
+	protected List<File> categoryFiles;
+	protected List<File> manifestFiles;
+
+	protected VersionUpdater() { }
 
 	public static void main(String[] args) throws Exception {
 		new VersionUpdater().update();
 	}
 
-	private void update() throws Exception {
+	protected void update() throws Exception {
 		determineNewOsgiVersion();
 
 		File rootDir = new File(this.rootDir);
 		collectFiles(rootDir);
+
 		updateCategoryFiles();
 		updateFeatureFiles();
 		updatePomFiles();
+		updateManifestFiles();
 	}
 
-	private void updatePomFiles() {
+	protected void updateManifestFiles() throws Exception {
+		for (File  f : manifestFiles) {
+			File tmpFile = new File(f.getParentFile(), f.getName() + ".tmp");
+			InputStream in = null;
+			OutputStream out = null;
+			try {
+				boolean replaceFile = false;
+				in = new FileInputStream(f);
+				out = new FileOutputStream(tmpFile);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+				Writer writer = new OutputStreamWriter(out, "UTF-8");
+				String line;
+				while (null != (line = reader.readLine())) {
+					if (line.startsWith("Bundle-SymbolicName:")) {
+						String bundleName = line.substring("Bundle-SymbolicName:".length()).trim();
+						if (bundleName.startsWith(artifactIdPrefix))
+							replaceFile = true;
+					}
+					else if (line.startsWith("Bundle-Version:")) {
+						line = "Bundle-Version: " + newOsgiVersionWithSnapshot;
+					}
+					writer.write(line);
+					writer.write("\r\n");
+				}
+				reader.close();
+				writer.close();
 
+				if (replaceFile) {
+					f.delete();
+					if (!tmpFile.renameTo(f))
+						throw new IOException("Failed to rename '" + tmpFile.getAbsolutePath() + "' to '" + f.getAbsolutePath() + "'!!!");
+				}
+			} finally {
+				if (in != null)
+					in.close();
+
+				if (out != null)
+					out.close();
+
+				tmpFile.delete();
+			}
+		}
 	}
 
-	private void updateFeatureFiles() throws Exception {
+	protected void updatePomFiles() {
+		// TODO implement WITHOUT loosing comments or layout information!!!
+		for (File  f : pomFiles) {
+
+		}
+	}
+
+	protected void updateFeatureFiles() throws Exception {
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 		TransformerFactory tFactory = TransformerFactory.newInstance();
@@ -68,13 +129,13 @@ public class VersionUpdater {
 		}
 	}
 
-	private void updateFeatureFileDocument(Document document) {
+	protected void updateFeatureFileDocument(Document document) {
 		NodeList featureList = document.getElementsByTagName("feature");
 		for (int i = 0; i < featureList.getLength(); ++i) {
 			Node node = featureList.item(i);
 			if (node instanceof Element) {
 				Element element = (Element) node;
-				element.setAttribute("version", newOsgiVersion);
+				element.setAttribute("version", newOsgiVersionWithQualifier);
 			}
 
 			NodeList childNodes = node.getChildNodes();
@@ -96,22 +157,24 @@ public class VersionUpdater {
 		}
 	}
 
-	private void determineNewOsgiVersion() {
+	protected void determineNewOsgiVersion() {
 		if (newMavenVersion.endsWith("-SNAPSHOT")) {
 			String versionWithoutSuffix = newMavenVersion.substring(0, newMavenVersion.length() - "-SNAPSHOT".length());
 			newOsgiVersionWithoutSuffix = versionWithoutSuffix;
-			newOsgiVersion = versionWithoutSuffix + ".qualifier";
+			newOsgiVersionWithQualifier = versionWithoutSuffix + ".qualifier";
+			newOsgiVersionWithSnapshot = versionWithoutSuffix + ".SNAPSHOT";
 		}
 		else  {
 			if (newMavenVersion.contains("-"))
 				throw new IllegalStateException("Maven version must not contain a suffix other than '-SNAPSHOT'!!! Other suffixes are not supported by tycho!");
 
 			newOsgiVersionWithoutSuffix = newMavenVersion;
-			newOsgiVersion = newMavenVersion;
+			newOsgiVersionWithQualifier = newMavenVersion;
+			newOsgiVersionWithSnapshot = newMavenVersion;
 		}
 	}
 
-	private void updateCategoryFiles() throws Exception {
+	protected void updateCategoryFiles() throws Exception {
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 		TransformerFactory tFactory = TransformerFactory.newInstance();
@@ -128,7 +191,7 @@ public class VersionUpdater {
 		}
 	}
 
-	private void updateCategoryFileDocument(Document document) {
+	protected void updateCategoryFileDocument(Document document) {
 		NodeList featureList = document.getElementsByTagName("feature");
 		for (int i = 0; i < featureList.getLength(); ++i) {
 			Node node = featureList.item(i);
@@ -145,20 +208,21 @@ public class VersionUpdater {
 					throw new IllegalStateException("url does not contain '_': " + url);
 
 				newUrl = newUrl.substring(0, lastUnderscoreIdx + 1);
-				newUrl = newUrl + newOsgiVersion + suffix;
+				newUrl = newUrl + newOsgiVersionWithQualifier + suffix;
 				element.setAttribute("url", newUrl);
-				element.setAttribute("version", newOsgiVersion);
+				element.setAttribute("version", newOsgiVersionWithQualifier);
 			}
 		}
 	}
 
-	private void collectFiles(File dir) {
+	protected void collectFiles(File dir) {
 		pomFiles = new ArrayList<File>();
 		featureFiles = new ArrayList<File>();
 		categoryFiles = new ArrayList<File>();
+		manifestFiles = new ArrayList<File>();
 		_collectFiles(dir);
 	}
-	private void _collectFiles(File dirOrFile) {
+	protected void _collectFiles(File dirOrFile) {
 		if ("pom.xml".equals(dirOrFile.getName())) {
 			pomFiles.add(dirOrFile);
 			return;
@@ -174,6 +238,11 @@ public class VersionUpdater {
 			return;
 		}
 
+		if ("MANIFEST.MF".equals(dirOrFile.getName())) {
+			manifestFiles.add(dirOrFile);
+			return;
+		}
+
 		File[] listFiles = dirOrFile.listFiles();
 		if (listFiles != null) {
 			for (File child : listFiles)
@@ -181,7 +250,7 @@ public class VersionUpdater {
 		}
 	}
 
-	private static String getTagValue(String sTag, Element eElement) {
+	protected static String getTagValue(String sTag, Element eElement) {
 		NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
 
 		Node nValue = nlList.item(0);
